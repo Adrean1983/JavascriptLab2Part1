f2549f93eff1654d15c4e717c42fd9f077cebe8e
Adrean1983 <adrean_@hotmail.com>
2018-07-29 21:18:56 -0400

    final commit

--- scripts/script.js
+++ scripts/script.js
@@ -12,76 +12,42 @@ class AddressBook {
         this.contacts.splice(index, 1);
         this.display();
     }
-    // total() {
-    //     // creates a variable to be used for keeping a running total
-    //     let total = 0;
-    //     // loop through the array of products
-    //     for (let contact of this.contacts) {
-    //       // increments total for each product's total price (qty * price). 
-    //       total += (contact.price * contact.qty);
-    //     }
-    //     // sets the textContent of the class total to be the value of the total variable
-    //     document.querySelector(".total").textContent = `${total}`;
-    // }
     display() {
-        // calls the clear method
         this.clear();
-        // this is the counter that is used as the delete button's argument
         let index = 0;
-        // loops through the array of products
         for (let contact of this.contacts) {
-          // creates a paragraph element for the product's name, price, and qty
-
-
-          const name = document.createElement("p");
-          const email = document.createElement("p");
-          const phone = document.createElement("p");
-          const relation = document.createElement("p");
-
-          
-          // sets the textContent of each of the newly created elements to be the values of the product's name, price, and qty
-          
-          
-          name.textContent = `Name: ${contact.name}`;
-          email.textContent = `Email: ${contact.email}`;
-          phone.textContent = `Phone: ${contact.phone}`;
-          relation.textContent = `Relation: ${contact.relation}`;
-          
-          
-          // selects the classes for name__column, price__column, and qty__column and appends the paragraphs created on lines 41-43
-          // Ask Sheamus
-          document.querySelector(".contactbox").appendChild(name);
-          document.querySelector(".contactbox").appendChild(email);
-          document.querySelector(".contactbox").appendChild(phone);
-          document.querySelector(".contactbox").appendChild(relation);
-
-          
-          // creates a section element to act as our container for our button
-          const deleteBtn = document.createElement("section");
-          // sets the innerHTML of the section element to contain a button. the button has an onclick attribute that will call the cart's delete method and pass in index as an argument.
-          // the argument index was created on line 37 
-          deleteBtn.innerHTML = `<button onclick="book.delete(${index});" class=".trashcontainer type="button"></button>`;
-          // appends the deleteBtn to the class of "btn__column"
-          document.querySelector(".trashcontainer").appendChild(deleteBtn);
-          // increment index by 1
-          index++;
+            const contactbox = document.createElement("section");
+            contactbox.className = "contactbox";
+            document.querySelector(".container2").appendChild(contactbox);
+            const name = document.createElement("p");
+            const email = document.createElement("p");
+            const phone = document.createElement("p");
+            const relation = document.createElement("p");
+            const trash = document.createElement("section");
+            trash.className = "trashcontainer";
+            name.textContent = `Name: ${contact.name}`;
+            email.textContent = `Email: ${contact.email}`;
+            phone.textContent = `Phone: ${contact.phone}`;
+            relation.textContent = `Relation: ${contact.relation}`;
+            contactbox.appendChild(name);
+            contactbox.appendChild(email);
+            contactbox.appendChild(phone);
+            contactbox.appendChild(relation);
+            contactbox.appendChild(trash);
+            trash.innerHTML = `<img onclick="book.delete(${index});" src="images/bin1.png"></img>`;
+            index++;
         }
-        // // calls the total method
-        // this.total();
     }
-      clearList() {
-        // initializes the products array to an empty array (empties/clears it)
+    clearList() {
         this.contacts = [];
-      }
-      clear() {
-        // selects the following classes: name__column, price__column, qty__column, btn__column, and total and sets the innerHTML of all of them to empty strings
-        document.querySelector(".name").innerHTML = "";
-        document.querySelector(".email").innerHTML = "";
-        document.querySelector(".phone").innerHTML = "";
-        document.querySelector(".relation").innerHTML = "";
-      }
     }
-
+    clear() {
+        var container2 = document.querySelector(".container2")
+        while (container2.firstChild) {
+            container2.removeChild(container2.firstChild);
+        }
+    }
+}
 class Contact {
     constructor(name, email, phone, relation) {
         this.name = name;
